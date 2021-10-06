cbae6ac4f372d4f4efeff1f3fb221d0978043aa9
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 18:40:45 -0400

    chore: add Montserrat font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,7 +45,7 @@ export default class MyDocument extends Document {
             crossOrigin=""
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&family=Montserrat:wght@700&display=swap"
             rel="stylesheet"
           />
         </Head>
