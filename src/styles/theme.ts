31aa885363f4f1543927b1f896fdcac8abb10957
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 18:08:51 -0400

    chore: add some theme tokens

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    brandPrimary: "#4F46BB",
+    brandPrimaryLoght: "#8E85FF",
+    textPrimary: "#302E45",
+    textSecondary: "#6D6C7B",
+    white: "#FFFFFF",
+    lightGray: "#F6F7FB",
+    grayDark: "#BBB8D9",
+    gray: " #F8F7FC",
+  },
+  rounded: {
+    full: "9999px",
+    lg: "0.5rem",
+  },
+} as const;
+
+export type Theme = typeof theme;
