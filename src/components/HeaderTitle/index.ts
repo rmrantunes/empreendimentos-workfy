970a08d25779cf6a29d7096d20aaad7db3e2c3d4
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 18:41:10 -0400

    refactor: HeaderTitle font-family as Montserrat

--- src/components/HeaderTitle/index.ts
+++ src/components/HeaderTitle/index.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const HeaderTitle = styled.h2`
   color: ${({ theme }) => theme.colors.brandPrimary};
 
-  /* font-family: Montserrat; */
+  font-family: Montserrat, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 `;
