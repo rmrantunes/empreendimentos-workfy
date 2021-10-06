8705b86ff539ec4660686a466421992552b9ce92
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 18:23:27 -0400

    refactor: remove mocked heading from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>Empreendimentos</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
