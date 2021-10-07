d5d93d4800ecebce43effda0a1a89f73129dda27
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 06:33:07 -0400

    refactor: HeaderTitle to be a strong

--- src/components/HeaderTitle/index.ts
+++ src/components/HeaderTitle/index.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
-export const HeaderTitle = styled.h2`
+export const HeaderTitle = styled.strong`
   color: ${({ theme }) => theme.colors.brandPrimary};
+  font-size: 1.5rem;
 
   font-family: Montserrat, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
     Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
