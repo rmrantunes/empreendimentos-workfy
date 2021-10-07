b2f897a24bfca4b3388177771495d2b245ae2545
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 08:34:46 -0400

    feat: shrink HeaderTitle size

--- src/components/HeaderTitle/index.ts
+++ src/components/HeaderTitle/index.ts
@@ -4,6 +4,10 @@ export const HeaderTitle = styled.strong`
   color: ${({ theme }) => theme.colors.brandPrimary};
   font-size: 1.5rem;
 
+  @media (max-width: 768px) {
+    font-size: 1.125rem;
+  }
+
   font-family: Montserrat, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
     Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 `;
