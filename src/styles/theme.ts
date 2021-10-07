4ba0b55eb6d10a8657ed241d297bfe44687ec14f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 22:08:39 -0400

    feat: add shadow token

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,6 +16,9 @@ export const theme = {
   duration: {
     fast: "0.2s",
   },
+  shadow: {
+    default: "0px 2px 4px rgba(48, 46, 69, 0.06)",
+  },
 } as const;
 
 export type Theme = typeof theme;
