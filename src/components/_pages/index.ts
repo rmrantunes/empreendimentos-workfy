936e6ad371b16e56bb0239a17012aa149c2ee5dd
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 04:45:34 -0400

    feat: add IndexPage components

---
+++ src/components/_pages/index.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  padding: 2.5rem 0;
+  display: grid;
+  gap: 3rem;
+`;
+
+export const EnterpiseList = styled.div`
+  display: grid;
+  gap: 2rem;
+`;
