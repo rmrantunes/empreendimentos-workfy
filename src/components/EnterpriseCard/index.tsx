60819f1d9c4581ebdb371fcd55636b5b9f48f0d4
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 07:12:20 -0400

    feat: EnterpriseCard purpose & status formatters

--- src/components/EnterpriseCard/index.tsx
+++ src/components/EnterpriseCard/index.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import { Enterprise, EnterprisePurpose, EnterpriseStatus } from "types";
 
 import { IconButton } from "components/IconButton";
 import { Badge } from "components/Badge";
 
 import PencilIcon from "assets/icons/pencil.svg";
 import TrashIcon from "assets/icons/trash.svg";
-import { Enterprise } from "types";
 
 import * as S from "./styles";
 
@@ -15,6 +15,28 @@ export type EnterpriseCardProps = {
 
 function formatAddress(address: Enterprise["address"]) {
   return `${address.street}, ${address.number} - ${address.district}, ${address.city}`;
+}
+
+function getStatusText(status: Enterprise["status"]) {
+  switch (status) {
+    case EnterpriseStatus.RELEASE:
+      return "Lançamento";
+    case EnterpriseStatus.RELEASE_SOON:
+      return "Breve lançamento";
+    case EnterpriseStatus.IN_CONSTRUCTION:
+      return "Em obras";
+    case EnterpriseStatus.READY:
+      return "Pronto pra morar";
+  }
+}
+
+function getPurposeText(status: Enterprise["purpose"]) {
+  switch (status) {
+    case EnterprisePurpose.HOME:
+      return "Residencial";
+    case EnterprisePurpose.BUSINESS:
+      return "Comercial";
+  }
 }
 
 export function EnterpriseCard(props: EnterpriseCardProps) {
@@ -33,8 +55,8 @@ export function EnterpriseCard(props: EnterpriseCardProps) {
         <S.Address>{formatAddress(props.enterprise.address)}</S.Address>
       </div>
       <S.Badges>
-        <Badge>Lançamento</Badge>
-        <Badge>Residencial</Badge>
+        <Badge>{getStatusText(props.enterprise.status)}</Badge>
+        <Badge>{getPurposeText(props.enterprise.purpose)}</Badge>
       </S.Badges>
     </S.Wrapper>
   );
