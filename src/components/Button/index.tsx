6a648a6142929bdc82d5f7515c5880a04124ece4
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 04:45:22 -0400

    feat: pass ButtonHTMLAttributes to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,19 @@
+import { ButtonHTMLAttributes } from "react";
 import * as S from "./styles";
 
-export type ButtonProps = {
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   icon?: React.ReactElement;
 };
 
 export function Button(props: ButtonProps) {
+  const { icon, children, ...buttonProps } = props;
+
   return (
-    <S.Wrapper>
+    <S.Wrapper {...buttonProps}>
       <S.Flex>
-        <span>{props.children}</span>
-        {props.icon && props.icon}
+        <span>{children}</span>
+        {icon && icon}
       </S.Flex>
     </S.Wrapper>
   );
