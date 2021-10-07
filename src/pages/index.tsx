56ff2badf555b26205398acb5de2fe146c116d56
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 04:46:06 -0400

    feat: finished Index page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+
+import { Container } from "components/Container";
+import { EnterpriseCard } from "components/EnterpriseCard";
+import { TextField } from "components/TextField";
+import { Button } from "components/Button";
+
+import SearchIcon from "assets/icons/search.svg";
+
+import * as S from "components/_pages/index";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <S.Main>
+        <TextField
+          icon={<Image src={SearchIcon} alt="" />}
+          placeholder="Buscar"
+        />
+        <S.EnterpiseList>
+          <EnterpriseCard />
+          <EnterpriseCard />
+        </S.EnterpiseList>
+        <Button style={{ justifySelf: "center" }}>Carregar mais</Button>
+      </S.Main>
+    </Container>
+  );
 };
 
 export default Home;
