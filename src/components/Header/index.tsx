8f1d0b106ce991fc7d332633809b5522486674a2
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 06:33:27 -0400

    feat: add back button to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,19 @@
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 
 import { Button } from "components/Button";
 import { Container } from "components/Container";
 import { HeaderTitle } from "components/HeaderTitle";
+import { IconButton } from "components/IconButton";
 
 import PlusIcon from "assets/icons/plus.svg";
+import ChevronLeft from "assets/icons/chevron-left.svg";
 
 import * as S from "./styles";
 
 export function Header() {
-  const { asPath, push } = useRouter();
+  const { asPath, push, back } = useRouter();
 
   const headerTitle = useMemo(() => {
     switch (asPath) {
@@ -29,7 +31,14 @@ export function Header() {
     <S.Wrapper>
       <Container>
         <S.SpaceBetween>
-          <HeaderTitle>{headerTitle}</HeaderTitle>
+          <S.SpaceBetween>
+            {asPath !== "/" && (
+              <IconButton title="Go to previous page" onClick={back}>
+                <Image src={ChevronLeft} alt="" />
+              </IconButton>
+            )}
+            <HeaderTitle>{headerTitle}</HeaderTitle>
+          </S.SpaceBetween>
 
           {asPath === "/" && (
             <Button
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -11,4 +11,8 @@ export const SpaceBetween = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  > * + * {
+    margin-left: 1rem;
+  }
 `;
