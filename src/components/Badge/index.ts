6861ec92c284c031baaf7e5e8b00ed05bf277751
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-07 07:02:13 -0400

    fix: add inline-block to Badge

--- src/components/Badge/index.ts
+++ src/components/Badge/index.ts
@@ -2,6 +2,8 @@ import styled, { css } from "styled-components";
 
 export const Badge = styled.span`
   ${({ theme }) => css`
+    display: inline-block;
+
     padding: 0.5rem 1.5rem;
     border-radius: ${theme.rounded.full};
     border: 1px solid ${theme.colors.brandPrimaryLight};
