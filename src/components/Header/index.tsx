5cbbfd8197d1e8ba26c77a19c5e0bfd79f90d8f9
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 20:02:03 -0400

    feat: add Button to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
+import { Button } from "components/Button";
 import { Container } from "components/Container";
 import { HeaderTitle } from "components/HeaderTitle";
+
 import * as S from "./styles";
 
 export function Header() {
@@ -8,6 +10,8 @@ export function Header() {
       <Container>
         <S.SpaceBetween>
           <HeaderTitle>Empreendimentos</HeaderTitle>
+
+          <Button>Adicionar</Button>
         </S.SpaceBetween>
       </Container>
     </S.Wrapper>
