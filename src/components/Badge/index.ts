6f4454b414006a0e2c1db6193dbfd7cb560732eb
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 20:08:21 -0400

    feat: add Badge component

---
+++ src/components/Badge/index.ts
@@ -0,0 +1,12 @@
+import styled, { css } from "styled-components";
+
+export const Badge = styled.span`
+  ${({ theme }) => css`
+    padding: 0.5rem 1.5rem;
+    border-radius: ${theme.rounded.full};
+    border: 1px solid ${theme.colors.brandPrimaryLight};
+
+    color: ${({ theme }) => theme.colors.textPrimary};
+    font-size: 0.875rem;
+  `}
+`;
