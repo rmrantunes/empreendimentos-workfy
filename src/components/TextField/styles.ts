812e6bb5c1866c4f147a754bd04a4e844fbb16dd
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-06 20:53:36 -0400

    feat: add TextField component

---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,26 @@
+import styled, { css } from "styled-components";
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    gap: 1rem;
+    font-size: 1rem;
+
+    border-bottom: 1px solid ${theme.colors.grayDark};
+
+    transition: ${theme.duration.fast};
+
+    :focus-within {
+      border-color: ${theme.colors.brandPrimaryLight};
+    }
+  `}
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  background: transparent;
+  border: none;
+  outline: none;
+
+  padding: 1rem 0;
+`;
