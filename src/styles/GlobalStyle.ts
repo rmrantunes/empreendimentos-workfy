4507f1a61c26e29326fd9006eb5e86903382e2b5
Rafael Antunes <rafaelmessias710@gmail.com>
2021-10-08 10:08:30 -0400

    feat: add .sr-only

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -41,6 +41,18 @@ const GlobalStyle = createGlobalStyle`
       background: ${({ theme }) => theme.colors.brandPrimaryLight};
     }
   }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
 `;
 
 export default GlobalStyle;
